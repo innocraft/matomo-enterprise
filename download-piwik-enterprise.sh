6db5a7335eb45cea1b8cfb168894ed98c686b6ec
Thomas Steur <tsteur@users.noreply.github.com>
2018-01-11 09:07:57 +1300

    Update download-piwik-enterprise.sh

--- download-piwik-enterprise.sh
+++ download-piwik-enterprise.sh
@@ -1,22 +1,22 @@
-LICENSE_KEY="Put your Piwik Marketplace license key here. Get it @ https://shop.piwik.org/my-account/"
+LICENSE_KEY="Put your Matomo Marketplace license key here. Get it @ https://shop.matomo.org/my-account/"
 
-mkdir "src" || { echo "Please delete the directory src/ before proceeding. Piwik Enterprise will be extracted in this directory." ; exit; }
+mkdir "src" || { echo "Please delete the directory src/ before proceeding. Matomo Enterprise will be extracted in this directory." ; exit; }
 
 PLUGINS_TO_DOWNLOAD="CustomDimensions CustomReports MarketingCampaignsReporting CustomAlerts LogViewer InvalidateReports TasksTimetable AbTesting MediaAnalytics FormAnalytics Funnels RollUpReporting SearchEngineKeywordsPerformance MultiChannelConversionAttribution HeatmapSessionRecording UsersFlow ActivityLog WhiteLabel"
 
 for PLUGIN_NAME in $PLUGINS_TO_DOWNLOAD
 do
     echo "Downloading plugin $PLUGIN_NAME..."
-    if curl -f -sS --data "access_token=$LICENSE_KEY" https://plugins.piwik.org/api/2.0/plugins/$PLUGIN_NAME/download/latest > plugins-$PLUGIN_NAME.zip; then
+    if curl -f -sS --data "access_token=$LICENSE_KEY" https://plugins.matomo.org/api/2.0/plugins/$PLUGIN_NAME/download/latest > plugins-$PLUGIN_NAME.zip; then
         echo "OK"
     else
-        echo -e "Please check your Piwik Marketplace license key is correct, and try again." >&2
+        echo -e "Please check your Matomo Marketplace license key is correct, and try again." >&2
         exit 1
     fi;
 done;
 
-echo -e "Downloading the Piwik Analytics platform..."
-curl https://builds.piwik.org/piwik.zip > piwik.zip
+echo -e "Downloading the Matomo Analytics platform..."
+curl https://builds.matomo.org/piwik.zip > piwik.zip
 
 echo -e "Extract all packages in the src/ directory..."
 
@@ -30,4 +30,4 @@ do
     unzip -q -o plugins-$PLUGIN_NAME -d src/plugins/
 done;
 
-echo -e "Done extracting files!\nPiwik Enterprise is packaged in src/"
+echo -e "Done extracting files!\nMatomo Enterprise is packaged in src/"
